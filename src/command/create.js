50b4e768cbc64cf6f1fb57ee5d9f1a505f28bfbe
zxyue25 <zxy@syzc.net.cn>
2021-03-05 11:55:10 +0800

    feat:create

--- src/command/create.js
+++ src/command/create.js
@@ -1,9 +1,88 @@
-const action = () => {
-   console.log("create")
+const path = require('path')
+const fs = require('fs-extra')
+const download = require('download-git-repo')
+const handlebars = require('handlebars')
+const ora = require('ora')
+const inquirer = require('inquirer')
+const logSymbols = require('log-symbols')
+const chalk = require('chalk')
+
+const downloadCode = async (projectName) => {
+    const projectPath = path.join(process.cwd(), projectName)
+    if(!await checkExist(projectName)){
+        return false
+    }
+    // 下载之前做loading提示
+    console.log(`✨  Creating project in ${chalk.cyan(projectPath)}.`)
+    console.log('🗃  Initializing git repository...')
+    const spinner = ora().start('This might take a while...\n\n');
+    // 根据模板名下载对应的模板到本地
+    const downloadUrl = "https://github.com:zxyue25/vue-demo-cli-templateA#master"
+    download(downloadUrl, projectName, {clone: true}, async err => {
+        if(err){
+          spinner.fail()
+          console.log(logSymbols.error, chalk.red(err))
+          return 
+        }
+        spinner.succeed()
+        answers = await inquirer.prompt([
+          {
+              type: 'input',
+              name: 'name',
+              message: `package name: (${projectName})`,
+              default: projectName
+          },
+          {
+              type: 'input',
+              name: 'description',
+              message: 'description'
+          },
+          {
+              type: 'input',
+              name: 'author',
+              message: 'author'
+          },
+        ])
+        const packagePath = `${projectName}/package.json`
+        const packageContent = fs.readFileSync(packagePath,'utf-8')
+        //使用handlebars解析模板引擎
+        const packageResult = handlebars.compile(packageContent)(answers)
+        //将解析后的结果重写到package.json文件中
+        fs.writeFileSync(packagePath,packageResult)
+        console.log(`🎉  Successfully created project ${chalk.yellow(projectName)}.`)
+        console.log(`👉  Get started with the following commands: \n`)
+        console.log(chalk.cyan(`$ cd ${projectName}\n$ npm run serve\n`))
+    })
+}
+
+const checkExist = async (projectName) => {
+    const projectPath = path.join(process.cwd(), projectName)
+    if(fs.existsSync(projectPath)){
+        const answer =  await inquirer.prompt({
+            type: 'list',
+            name: 'checkExist',
+            message: `Target directory ${projectPath} already exists. Pick an action`,
+            choices: [
+                'Overwrite',
+                'Cancel'
+            ]
+        })
+        if(answer.checkExist === 'Overwrite'){
+            console.log(`Removing ${chalk.cyan(projectPath)}...\n\n`)
+            fs.removeSync(projectPath)
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+const action = (projectName) => {
+   downloadCode(projectName)
 }
 
 module.exports = {
-   command: 'create',
+   command: 'create <projectName>',
    description: '初始化',
    action: action,
 }
